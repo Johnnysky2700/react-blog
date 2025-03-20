1ef844a4d517442ca0bcefd8b5803e1bb247d319
iamjohnnysky <vanpasie20@gmail.com>
2025-03-20 16:50:33 -0700

    Functions as props(delete an items)

--- src/Home.js
+++ src/Home.js
@@ -7,10 +7,14 @@ const Home = () => {
         { title: 'Welcome party', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return ( 
         <div className="home">
-            <BlockList blogs={blogs} title="All Blogs!"/>
-            <BlockList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Johnnysky's blogs"/>
+            <BlockList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
         </div>
      );
 }
