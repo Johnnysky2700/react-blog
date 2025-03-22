d3abddc2aef5a6e1052a1e1f5c538b3d6539cec5
iamjohnnysky <vanpasie20@gmail.com>
2025-03-22 14:38:08 -0700

    useEffect Dependencies

--- src/Home.js
+++ src/Home.js
@@ -8,19 +8,25 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
 
     useEffect(() => {
-        console.log('use effect run')
-        console.log(blogs);
-    });
+        console.log('use effect run');
+        console.log(name);
+    }, [name]);
+
     return ( 
         <div className="home">
             <BlockList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{ name }</p>
         </div>
      );
 }
+
 export default Home;
