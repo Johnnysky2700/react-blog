3293236ab94aab62ce56cfae6077e5701c2a2d9a
iamjohnnysky <vanpasie20@gmail.com>
2025-03-23 15:23:16 -0700

    conditional templates

--- src/Home.js
+++ src/Home.js
@@ -4,20 +4,24 @@ import BlockList from './BlogList';
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
 
-    const [name, setName] = useState('mario');
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-       fetch('http://localhost:8000/blogs')
+       setTimeout(()=> {
+         fetch('http://localhost:8000/blogs')
          .then(res => {
             return res.json()
          })
          .then(data => {
             setBlogs(data);
-         })
+            setIsPending(false);
+         });
+       }, 1000);
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {blogs && <BlockList blogs={blogs} title="All Blogs!"/>}
         </div>
      );
